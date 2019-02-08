ef5825dd9e2e741af488e17f566df8e5af9579f7
Roman Pidkostelnyi <pidkostelnyi@gmail.com>
2019-02-08 17:27:18 +0200

    templates

--- scripts/Classes/TestOption.js
+++ scripts/Classes/TestOption.js
@@ -5,27 +5,22 @@ var TestOption = core.define(
   function($root, description) {
     this.$root = $root;
     this.description = description;
+
+    this.template =
+      '<label>'+
+        '<%- title %>'+
+        '<input type="radio" name="q<%- index %>" value="<%- index %>">'+
+      '</label>';
   },
   
   {
     render: function() {
-      var label = document.createElement('label');
+      var optionMarkup = _.template(this.template)({
+        title: this.description.title,
+        index: this.description.value
+      });
 
-      var attrs = {
-        type: 'radio',
-        name: this.description.name,
-        value: this.description.value,
-      };
-      
-      this.$el = document.createElement('input');
-      label.innerHTML = this.description.title;
-
-      Object.keys(attrs).forEach(function(attr) {
-        this.$el.setAttribute(attr, attrs[attr]);
-      }.bind(this));
-
-      label.appendChild(this.$el);
-      this.$root.appendChild(label);
+      this.$root.innerHTML += optionMarkup;
 
       return this;
     },
