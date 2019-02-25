877847ea58cf1dfaf0f46dd527378d593742292b
Roman Pidkostelnyi <pidkostelnyi@gmail.com>
2019-02-25 17:26:35 +0200

    EDITABLE LIST

--- src/List.js
+++ src/List.js
@@ -1,18 +1,59 @@
-import React, { Component } from 'react';
+import React, { Component, createRef } from 'react';
 
 class List extends Component {
     constructor(props){
         super(props);
 
-        console.log(props);
+        this.state = {
+            items: this.props.items
+        };
+
+        this.inputRef = createRef();
+
+        this.addItem = this.addItem.bind(this);
+    }
+
+    addItem() {
+        const {items} = this.state;
+        items.push({ label: this.inputRef.current.value })
+
+        this.setState({ items });
     }
 
     render() {
-       return (<ul>
-           {
-               this.props.items.map(item => <li key={Math.round(Math.random() * 100)}>{item.label}</li>)
-           }
-       </ul>);
+       const { items } = this.state;
+
+       return (
+        <div className="list">
+            <div className="input-group">
+                <input
+                    type="text"
+                    className="form-control"
+                    ref={this.inputRef}
+                />
+                <button
+                    className="btn btn-outline-secondary"
+                    type="button"
+                    onClick={this.addItem}
+                >
+                    Add Item
+                </button>
+            </div>
+            
+            <ul className="list-group">
+            {
+                items.map((item, i) => (
+                <li
+                    key={i}
+                    className="list-group-item"
+                >
+                    {item.label}
+                </li>
+                ))
+            }
+            </ul>
+        </div>
+       );
     }
 }
 
