83606ff51f2a40570f7f553c82e32b7f4ae9f2c0
andrii.pylypenko <andrii.pylypenko@altexsoft.com>
2019-02-25 10:19:55 +0200

    add item

--- src/List.js
+++ src/List.js
@@ -1,18 +1,46 @@
-import React, { Component } from 'react';
+import React, { Component, createRef } from 'react';
 
 class List extends Component {
     constructor(props){
         super(props);
 
-        console.log(props);
+        this.state = { items: this.props.items };
+
+        this.inputField = createRef();
+
+        this.addItem = this.addItem.bind(this);
+    }
+
+    addItem() {
+        const inputField = this.inputField.current;
+
+        if (!inputField || !inputField.value) {
+            return;
+        }
+
+        const { items } = this.state;
+
+        items.push({ label: inputField.value });
+
+        this.setState({ items });
+        inputField.value = null;
     }
 
     render() {
-       return (<ul>
-           {
-               this.props.items.map(item => <li key={Math.round(Math.random() * 100)}>{item.label}</li>)
-           }
-       </ul>);
+        const { items } = this.state;
+
+       return (
+        <div>
+            <ul>
+                {
+                    items.map((item, index) => <li key={index}>{item.label}</li>)
+                }
+            </ul>
+
+            <input type="text" ref={this.inputField} />
+            <button type="button" onClick={this.addItem}>Add</button>
+        </div>
+       );
     }
 }
 
